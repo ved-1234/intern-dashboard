7de82acc6e2e0efc1780bc12e274ecdeb2351b19
ved-1234 <ved.221473101@vcet.edu.in>
2026-02-23 12:03:50 +0530

    updated frontend

--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -27,7 +27,7 @@ export const useAuthStore = create<AuthState>((set) => ({
     set({ isLoading: true, error: null });
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
+      const res = await fetch("https://intern-backend1.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
@@ -63,7 +63,7 @@ export const useAuthStore = create<AuthState>((set) => ({
     set({ isLoading: true, error: null });
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/register", {
+      const res = await fetch("https://intern-backend1.onrender.com/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ FullName, email, password }),
